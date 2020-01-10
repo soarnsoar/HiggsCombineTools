393e151d72f551c1b850d90d221576c0ceb8a4c7
your name your last name <soarnsoar@gmail.com>
2020-01-10 13:41:40 +0900

    scan dir

--- GetExpectedLimits.sh
+++ GetExpectedLimits.sh
@@ -1,11 +1,12 @@
-### under ../Datacard_M$$
-
-source DefineMassBin.sh ##ARR_MAss=(mass1 mass2 ...)
+ARR_DIR=($(ls -d Datacard_M*/))
 
 
 
+for DIR in ${ARR_DIR[@]};do
+    MASS=${DIR%\/}
+    MASS=${MASS#Datacard_M}
+    echo $MASS
 
-for MASS in ${ARR_MASS[@]};do
     echo "Get Expected Limit of M${MASS}"
     combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/combine_M${MASS}.txt &> ExpectedLimit_M${MASS}.txt
 
