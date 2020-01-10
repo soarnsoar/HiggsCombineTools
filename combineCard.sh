7345c7007ce54d147c400e9ef40e8db27b94805f
your name your last name <soarnsoar@gmail.com>
2020-01-10 19:11:35 +0900

    for 3year

--- combineCard.sh
+++ combineCard.sh
@@ -1,14 +1,28 @@
 #source DefineMassBin.sh ##ARR_MASS=(mass1 mass2 ...)
+#Datacard_M200/BoostedSR2017
 
-ARR_DIR=($(ls -d Datacard_M*/))
+ARR_CARDDIR_YEAR=($(ls -d Datacards_*/))
+
+for CARDDIR in ${ARR_CARDDIR_YEAR[@]};do
+
+    YEAR=${CARDDIR%\/}
+    YEAR=${YEAR#Datacards_}
+    echo "YEAR="$YEAR
+    
+  
+    pushd ${CARDDIR}
 
 
+    ARR_DIR=($(ls -d Datacard_M*/))
+    for DIR in ${ARR_DIR[@]};do
+	MASS=${DIR%\/}
+	MASS=${MASS#Datacard_M}
+	echo $MASS
+	
+	combineCards.py -S MuonChggfBoostedSR=Datacard_M${MASS}/MuonChggfBoostedSR${YEAR}/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt ElectronChggfBoostedSR=Datacard_M${MASS}/ElectronChggfBoostedSR${YEAR}/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt MuonChggfResolvedSR=Datacard_M${MASS}/MuonChggfResolvedSR/LnJJ_mass${YEAR}/datacard.txt ElectronChggfResolvedSR=Datacard_M${MASS}/ElectronChggfResolvedSR${YEAR}/LnJJ_mass/datacard.txt &> Datacard_M${MASS}/combine_M${MASS}.txt
+	
+    done
 
-for DIR in ${ARR_DIR[@]};do
-    MASS=${DIR%\/}
-    MASS=${MASS#Datacard_M}
-    echo $MASS
-
-    combineCards.py -S MuonChggfBoostedSR=Datacard_M${MASS}/MuonChggfBoostedSR/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt ElectronChggfBoostedSR=Datacard_M${MASS}/ElectronChggfBoostedSR/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt MuonChggfResolvedSR=Datacard_M${MASS}/MuonChggfResolvedSR/LnJJ_mass/datacard.txt ElectronChggfResolvedSR=Datacard_M${MASS}/ElectronChggfResolvedSR/LnJJ_mass/datacard.txt &> Datacard_M${MASS}/combine_M${MASS}.txt
+    popd
 
 done
