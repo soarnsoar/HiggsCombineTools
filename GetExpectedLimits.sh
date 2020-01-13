b4c8b0ed3d247f467719a9322aa8438a6b1a8216
your name your last name <soarnsoar@gmail.com>
2020-01-13 13:04:32 +0900

    auto find year

--- GetExpectedLimits.sh
+++ GetExpectedLimits.sh
@@ -7,10 +7,10 @@ ARR_DIR=($(ls -d Datacard_M*/))
 for DIR in ${ARR_DIR[@]};do
     MASS=${DIR%\/}
     MASS=${MASS#Datacard_M}
-
+    YEAR=${PWD#*Datacards_}
     echo "Get Expected Limit of M${MASS}"
     combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/combine_M${MASS}.txt &> ExpectedLimit_combine_M${MASS}.txt
-    combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/BoostedSR/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt &> ExpectedLimit_BoostedSR_M${MASS}.txt
-    combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/ResolvedSR/LnJJ_mass/datacard.txt &> ExpectedLimit_ResolvedSR_M${MASS}.txt
+    combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/BoostedSR${YEAR}/CleanFatJetPassMBoostedSR_HlnFat_mass/datacard.txt &> ExpectedLimit_BoostedSR_M${MASS}.txt
+    combine -M AsymptoticLimits --bypassFrequentistFit --run blind Datacard_M${MASS}/ResolvedSR${YEAR}/LnJJ_mass/datacard.txt &> ExpectedLimit_ResolvedSR_M${MASS}.txt
 
 done
